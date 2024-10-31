9cada5b826563dcc00ef4c1031d696a703e5c0b6
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-10-31 11:09:59 +0900

    fix start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ Xvfb ${DISPLAY} -screen 0 1280x720x24 &
 x11vnc -display ${DISPLAY} -xkb -forever -shared -repeat -listen 0.0.0.0 -nopw -reopen &
 /usr/bin/websockify --web=/usr/share/novnc/ 0.0.0.0:8080 localhost:5900 &
 
-export SHELL=${CONTAINER_SHELL}
+export SHELL="/bin/bash"
 
 startxfce4
